0e6cf61cf412f1746ba985048a722013a366410d
Thomas Finnerty <34747130+tfinnerty02@users.noreply.github.com>
2019-11-14 09:05:11 -0500

    third commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -5,11 +5,36 @@ public class BankAccount {
     private double balance;
     private User accountHolder;
 
-    ////////////////////////////////////////////////////////////////////////////
-    //                                                                        //
-    // Refer to the Simple ATM tutorial to fill in the details of this class. //
-    //                                                                        //
-    ////////////////////////////////////////////////////////////////////////////
+    public BankAccount(int pin, long accountNo, double balance, User accountHolder) {
+        this.pin = pin;
+        this.accountNo = accountNo;
+        this.balance = balance;
+        this.accountHolder = accountHolder;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public long getAccountNo() {
+        return accountNo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public User getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount) {
+        balance = balance - amount;
+    }
 
     /*
      * Formats the account balance in preparation to be written to the data file.
